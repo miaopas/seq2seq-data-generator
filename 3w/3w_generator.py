f7c2a36967597666730007058e0664985d79c8b8
sanderwang <25528016+radarFudan@users.noreply.github.com>
2023-04-21 20:33:29 +0800

    3w from zhou

---
+++ 3w/3w_generator.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# 3w dataset generator for a binary classification task with multivariable input
+# Please clone the repository: https://github.com/petrobras/3W.git
+# Original dataset can be find under dataset directory, 
+# folder 0 contains nomal condition, while folder 4 represent flow instability fault. 
+# Please put the script 3w_generator.py under 3W-main, outside dataset folder.
+import numpy as np
+import pandas as pd
+import os
+
+
+
+normal_path = 'dataset\\0\\'
+inst_path = 'dataset\\4\\' # instability data
+normal_name = os.listdir(normal_path)
+inst_name = os.listdir(inst_path)
+
+# for test
+# normal_name = normal_name[:10]
+# inst_name = inst_name[:10]
+
+
+def split_dataset(filelist, train_test_ratio):
+    split = int(len(filelist)*train_test_ratio)
+    np.random.shuffle(filelist)
+    trainlist = filelist[:split]
+    testlist = filelist[split:]
+    return trainlist, testlist
+
+
+def overlap_window(data, length, overlap_ratio = 0, sliding_window = False):
+    if overlap_ratio != 0 and sliding_window == True:
+        raise ValueError('Use overlap_ratio OR sliding_window')
+    start  = 0
+    dataset = []
+    if sliding_window:
+        while start+length < data.shape[0]:
+            dataset.append(data[int(start):int(start+length)])
+            start += 1
+    else:
+        while start+length < data.shape[0]:
+            dataset.append(data[int(start):int(start+length)])
+            start = int(start+ length * (1-overlap_ratio))        
+    dataset = np.array(dataset)
+    return dataset
+
+
+def read_files(filepath, filelist, length, overlap_ratio = 0, sliding_window = False):
+    for idx, file in enumerate(filelist):   
+        path = os.path.join(filepath,file)
+        data = pd.read_csv(path, sep = ',')
+        
+        p_tpt = data['P-TPT']
+        t_tpt = data['T-TPT']
+        p_ckp = data['P-MON-CKP']
+        
+        p_tpt = overlap_window(p_tpt, length, overlap_ratio, sliding_window)
+        t_tpt = overlap_window(t_tpt, length, overlap_ratio, sliding_window)
+        p_ckp = overlap_window(p_ckp, length, overlap_ratio, sliding_window)
+        
+        if idx == 0:
+            train = np.stack([p_tpt, t_tpt, p_ckp], axis = 2)
+        
+        else:
+            temp = np.stack([p_tpt, t_tpt, p_ckp], axis = 2)
+            train = np.concatenate((train, temp), axis = 0)
+    
+    return train
+
+def w3_generator(length, train_test_ratio, overlap_ratio = 0, sliding_window = False):
+    # split normal dataset
+    trainlist_normal, testlist_normal = split_dataset(normal_name, train_test_ratio)
+    # split instable dataset
+    trainlist_inst, testlist_inst = split_dataset(inst_name, train_test_ratio)
+    
+    # train dataset
+    # build normal train dataset
+    train_normal = read_files(normal_path, trainlist_normal, length, overlap_ratio, sliding_window)
+    train_normal_output = np.zeros(train_normal.shape[0])
+    # build instable train dataset
+    train_inst = read_files(inst_path, trainlist_inst, length, overlap_ratio, sliding_window)
+    train_inst_output = np.ones(train_inst.shape[0])
+    # combine
+    train = np.concatenate((train_normal, train_inst), axis = 0)
+    train_output = np.concatenate((train_normal_output, train_inst_output), axis = 0)
+    # shffle
+    state1 = np.random.get_state()
+    np.random.shuffle(train)
+    np.random.set_state(state1)
+    np.random.shuffle(train_output)
+    
+    # test dataset
+    # build normal test dataset
+    test_normal = read_files(normal_path, testlist_normal, length, overlap_ratio, sliding_window)
+    test_normal_output = np.zeros(test_normal.shape[0])
+    # build instable test dataset
+    test_inst = read_files(inst_path, testlist_inst, length, overlap_ratio, sliding_window)
+    test_inst_output = np.ones(test_inst.shape[0])
+    # combine 
+    test = np.concatenate((test_normal,test_inst), axis = 0)
+    test_output = np.concatenate((test_normal_output,test_inst_output), axis = 0)
+    # shffle
+    state2 = np.random.get_state()
+    np.random.shuffle(test)
+    np.random.set_state(state2)
+    np.random.shuffle(test_output)
+    
+    return train, train_output, test, test_output
+        
+# default length = 900 for a 15 mins time window
+if __name__ == "__main__":
+    train_input, train_output, test_input, test_output = w3_generator(900, 0.7, overlap_ratio = 0)
