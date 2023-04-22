bc42e1ee3dcee3b05f7126571c49718419250eb5
sanderwang <25528016+radarFudan@users.noreply.github.com>
2023-04-22 13:21:16 +0800

    code at the start of 23.04.22

--- libs/train.py
+++ libs/train.py
@@ -1,6 +1,10 @@
 from pytorch_lightning import Trainer
 import torch
-from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor, EarlyStopping
+from pytorch_lightning.callbacks import (
+    ModelCheckpoint,
+    LearningRateMonitor,
+    EarlyStopping,
+)
 from pytorch_lightning.strategies import DDPStrategy
 from pytorch_lightning.loggers import TensorBoardLogger
 import os
@@ -12,9 +16,22 @@ from datetime import datetime
 from ml_collections import FrozenConfigDict
 from libs.lfgenerator import Shift
 import numpy as np
+
 # from libs.seq2seq_model import S4DModel, S4Model
 
-def train_model(name, model, input, output, train_test_split, epochs=300, batch_size=128, check_point_monitor='valid_loss', devices=4, call_backs=None):
+
+def train_model(
+    name,
+    model,
+    input,
+    output,
+    train_test_split,
+    epochs=300,
+    batch_size=128,
+    check_point_monitor="valid_loss",
+    devices=4,
+    call_backs=None,
+):
     """_summary_
 
     Args:
@@ -25,7 +42,7 @@ def train_model(name, model, input, output, train_test_split, epochs=300, batch_
         train_test_split (float): ratio of train test split
     """
     if input is not None:
-    # If input not provided then skip this part
+        # If input not provided then skip this part
         if not isinstance(input, torch.Tensor):
             input = torch.tensor(input, dtype=torch.float64)
         if not isinstance(output, torch.Tensor):
@@ -33,61 +50,97 @@ def train_model(name, model, input, output, train_test_split, epochs=300, batch_
 
         dataset = torch.utils.data.TensorDataset(input, output)
         total = len(dataset)
-        train_size = floor(total*train_test_split)
+        train_size = floor(total * train_test_split)
         test_size = total - train_size
 
-        train_dataset, valid_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,drop_last=True, num_workers=os.cpu_count(), pin_memory=True)
-        valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=batch_size,drop_last=True, num_workers=os.cpu_count(), pin_memory=True)
+        train_dataset, valid_dataset = torch.utils.data.random_split(
+            dataset, [train_size, test_size]
+        )
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset,
+            batch_size=batch_size,
+            drop_last=True,
+            num_workers=os.cpu_count(),
+            pin_memory=True,
+        )
+        valid_loader = torch.utils.data.DataLoader(
+            valid_dataset,
+            batch_size=batch_size,
+            drop_last=True,
+            num_workers=os.cpu_count(),
+            pin_memory=True,
+        )
     else:
         train_loader = None
         valid_loader = None
 
-    lr_monitor = LearningRateMonitor(logging_interval='epoch')
+    lr_monitor = LearningRateMonitor(logging_interval="epoch")
     now = datetime.now().strftime("%H:%M:%S__%m-%d")
     checkpoint_callback = ModelCheckpoint(
-                                        save_top_k=5, 
-                                        monitor=check_point_monitor,
-                                        filename=name + "-{epoch:02d}-{valid_loss:.2e}") 
-
+        save_top_k=5,
+        monitor=check_point_monitor,
+        filename=name + "-{epoch:02d}-{valid_loss:.2e}",
+    )
 
     default_callbacks = [checkpoint_callback, lr_monitor] + call_backs
 
     if devices == 1:
-        trainer = Trainer(accelerator="gpu", 
-                    devices=[3],
-                    max_epochs=epochs,
-                    precision=64,
-                    logger=TensorBoardLogger("runs", name=name),
-                    callbacks=default_callbacks)
+        trainer = Trainer(
+            accelerator="gpu",
+            devices=[3],
+            max_epochs=epochs,
+            precision=64,
+            logger=TensorBoardLogger("runs", name=name),
+            callbacks=default_callbacks,
+        )
     else:
-        trainer = Trainer(accelerator="gpu", 
-                    devices=devices,
-                    strategy=DDPStrategy(find_unused_parameters=False),
-                    max_epochs=epochs,
-                    precision=64,
-                    logger=TensorBoardLogger("runs", name=name),
-                    callbacks=default_callbacks)
+        trainer = Trainer(
+            accelerator="gpu",
+            devices=devices,
+            strategy=DDPStrategy(find_unused_parameters=False),
+            max_epochs=epochs,
+            precision=64,
+            logger=TensorBoardLogger("runs", name=name),
+            callbacks=default_callbacks,
+        )
 
-    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=valid_loader)
+    trainer.fit(
+        model=model, train_dataloaders=train_loader, val_dataloaders=valid_loader
+    )
 
     return trainer.validate(model=model, dataloaders=train_loader)
 
+
 def shift_rnn():
-    shifts = [10,20,30,40,50,60,70,80,90,100]
+    shifts = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     res = []
     for s in shifts:
         hid_dim = 32
-        model = ComplexLinearRNNModel(hid_dim,1,1)
-        generator = Shift({'input_dim':1, 'path_len':128 ,'shift':[s], 'data_num':100000})
+        model = ComplexLinearRNNModel(hid_dim, 1, 1)
+        generator = Shift(
+            {"input_dim": 1, "path_len": 128, "shift": [s], "data_num": 100000}
+        )
 
         x, y = generator.generate()
-        early_stop_callback = EarlyStopping(monitor="train_loss",min_delta=1e-10, patience=3, verbose=False, mode="min")
-        res.append(train_model('rnn_shift', model, x, y, 0.8, epochs=400, devices=4, call_backs=[early_stop_callback]) )
+        early_stop_callback = EarlyStopping(
+            monitor="train_loss", min_delta=1e-10, patience=3, verbose=False, mode="min"
+        )
+        res.append(
+            train_model(
+                "rnn_shift",
+                model,
+                x,
+                y,
+                0.8,
+                epochs=400,
+                devices=4,
+                call_backs=[early_stop_callback],
+            )
+        )
 
     import pickle
 
-    with open('res.pickle', 'wb') as handle:
+    with open("res.pickle", "wb") as handle:
         pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
@@ -96,26 +149,30 @@ def shift_rnn_complex():
     res = []
     for s in shifts:
         hid_dim = 32
-        model = LinearRNNModel(hid_dim,1,1)
-        generator = Shift({'input_dim':1, 'path_len':128 ,'shift':[s], 'data_num':100000})
+        model = LinearRNNModel(hid_dim, 1, 1)
+        generator = Shift(
+            {"input_dim": 1, "path_len": 128, "shift": [s], "data_num": 100000}
+        )
 
         x, y = generator.generate()
         # early_stop_callback = EarlyStopping(monitor="train_loss",min_delta=1e-10, patience=3, verbose=False, mode="min")
         # res.append(train_model('rnn_shift', model, x, y, 0.8, epochs=400, devices=4, call_backs=[early_stop_callback]) )
-        res.append(train_model('rnn_shift', model, x, y, 0.8, epochs=400, devices=4, call_backs=[]) )
+        res.append(
+            train_model(
+                "rnn_shift", model, x, y, 0.8, epochs=400, devices=4, call_backs=[]
+            )
+        )
 
     import pickle
 
-    with open('res.pickle', 'wb') as handle:
+    with open("res.pickle", "wb") as handle:
         pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+
 def train_exp():
-    
     hid_dim = 12
-    model = LinearRNNModel(hid_dim,1,1)
-    generator = Exponential({'input_dim':1, 'path_len':128 ,'data_num':10000})
-    
-    
+    model = LinearRNNModel(hid_dim, 1, 1)
+    generator = Exponential({"input_dim": 1, "path_len": 128, "data_num": 10000})
 
     x, y = generator.generate()
 
@@ -124,10 +181,10 @@ def train_exp():
     permuted[-5:] = x[:5]
 
     x = permuted
-    early_stop_callback = EarlyStopping(monitor="train_loss",min_delta=1e-10, patience=3, verbose=False, mode="min")
-    train_model('exp', model, x, y, 0.8, epochs=400, devices=4, call_backs=[]) 
-
-
+    early_stop_callback = EarlyStopping(
+        monitor="train_loss", min_delta=1e-10, patience=3, verbose=False, mode="min"
+    )
+    train_model("exp", model, x, y, 0.8, epochs=400, devices=4, call_backs=[])
 
 
 def shift_s4d():
@@ -135,19 +192,24 @@ def shift_s4d():
     res = []
     for s in shifts:
         hid_dim = 32
-        model = S4DModel(hid_dim=hid_dim,output_dim=1)
-        generator = Shift({'input_dim':1, 'path_len':5 ,'shift':[s], 'data_num':100000})
+        model = S4DModel(hid_dim=hid_dim, output_dim=1)
+        generator = Shift(
+            {"input_dim": 1, "path_len": 5, "shift": [s], "data_num": 100000}
+        )
 
         x, y = generator.generate()
         # early_stop_callback = EarlyStopping(monitor="train_loss",min_delta=1e-10, patience=3, verbose=False, mode="min")
         # res.append(train_model('rnn_shift', model, x, y, 0.8, epochs=400, devices=4, call_backs=[early_stop_callback]) )
-        res.append(train_model('s4d_shift', model, x, y, 0.8, epochs=400, devices=1, call_backs=[]) )
+        res.append(
+            train_model(
+                "s4d_shift", model, x, y, 0.8, epochs=400, devices=1, call_backs=[]
+            )
+        )
 
     import pickle
 
-    with open('res.pickle', 'wb') as handle:
+    with open("res.pickle", "wb") as handle:
         pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
 
 
 def shift_s4():
@@ -155,15 +217,120 @@ def shift_s4():
     res = []
     for s in shifts:
         hid_dim = 32
-        model = S4Model(hid_dim=hid_dim,output_dim=1)
-        generator = Shift({'input_dim':1, 'path_len':128 ,'shift':[s], 'data_num':100000})
+        model = S4Model(hid_dim=hid_dim, output_dim=1)
+        generator = Shift(
+            {"input_dim": 1, "path_len": 128, "shift": [s], "data_num": 100000}
+        )
 
         x, y = generator.generate()
         # early_stop_callback = EarlyStopping(monitor="train_loss",min_delta=1e-10, patience=3, verbose=False, mode="min")
         # res.append(train_model('rnn_shift', model, x, y, 0.8, epochs=400, devices=4, call_backs=[early_stop_callback]) )
-        res.append(train_model('s4d_shift', model, x, y, 0.8, epochs=400, devices=1, call_backs=[]) )
+        res.append(
+            train_model(
+                "s4d_shift", model, x, y, 0.8, epochs=400, devices=1, call_backs=[]
+            )
+        )
 
     import pickle
 
-    with open('res.pickle', 'wb') as handle:
+    with open("res.pickle", "wb") as handle:
         pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def train_3w(
+    name,
+    model,
+    x_train,
+    y_train,
+    x_test,
+    y_test,
+    epochs=300,
+    batch_size=128,
+    check_point_monitor="valid_loss",
+    devices=1,
+    call_backs=None,
+    dtype=torch.float64,
+):
+    if not isinstance(x_train, torch.Tensor):
+        x_train = torch.tensor(x_train, dtype=dtype)
+    if not isinstance(y_train, torch.Tensor):
+        y_train = torch.tensor(y_train, dtype=dtype)
+    if not isinstance(x_test, torch.Tensor):
+        x_test = torch.tensor(x_test, dtype=dtype)
+    if not isinstance(y_test, torch.Tensor):
+        y_test = torch.tensor(y_test, dtype=dtype)
+
+    print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+    train_dataset = torch.utils.data.TensorDataset(x_train, y_train)
+    test_dataset = torch.utils.data.TensorDataset(x_test, y_test)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        drop_last=True,
+        num_workers=os.cpu_count(),
+        pin_memory=True,
+    )
+    valid_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        drop_last=True,
+        num_workers=os.cpu_count(),
+        pin_memory=True,
+    )
+
+    lr_monitor = LearningRateMonitor(logging_interval="epoch")
+    now = datetime.now().strftime("%H:%M:%S__%m-%d")
+    checkpoint_callback = ModelCheckpoint(
+        save_top_k=5,
+        monitor=check_point_monitor,
+        filename=name + "-{epoch:02d}-{valid_loss:.2e}",
+    )
+
+    default_callbacks = [checkpoint_callback, lr_monitor] + call_backs
+
+    if devices == 1:
+        trainer = Trainer(
+            accelerator="gpu",
+            devices=[3],
+            max_epochs=epochs,
+            precision=64,
+            logger=TensorBoardLogger("runs", name=name),
+            callbacks=default_callbacks,
+        )
+    else:
+        trainer = Trainer(
+            accelerator="gpu",
+            devices=devices,
+            strategy=DDPStrategy(find_unused_parameters=False),
+            max_epochs=epochs,
+            precision=64,
+            logger=TensorBoardLogger("runs", name=name),
+            callbacks=default_callbacks,
+        )
+
+    model.eval()
+    loss = 0
+    for x, y in train_loader:
+        # print(x.shape, y.shape)
+        print("input", torch.isnan(x))
+        print("input", np.linalg.norm(x))
+        # print(sum(model(x)))
+        print("pred", model(x))
+        loss_tmp = torch.nn.MSELoss()(model(x), y)
+        print("loss_tmp", loss_tmp)
+        loss += loss_tmp
+        exit()
+
+    print(loss / len(train_loader))
+
+    # Validation before training
+    trainer.validate(model=model, dataloaders=train_loader)
+
+    print("Start training")
+
+    trainer.fit(
+        model=model, train_dataloaders=train_loader, val_dataloaders=valid_loader
+    )
+
+    return trainer.validate(model=model, dataloaders=train_loader)
--- run.py
+++ run.py
@@ -1,5 +1,76 @@
-# from libs.train import *
-from libs.train_with_tune import *
+import os
+import time
+from pathlib import Path
 
-tune_linear_cnn()
-# train_linear_cnn()
+from libs.train import *
+from code_3w.data_3w_generator import w3_generator
+
+# from libs.train_with_tune import *
+
+
+if __name__ == "__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 2, 3"
+
+    # tune_linear_cnn()
+    # train_linear_cnn()
+
+    # According to the default parameters in 3w_generator.py, the sequence length is 900
+    # First time to run this code, the data will be generated and saved in the data folder
+
+    T = 900
+    data_dir = Path(f"./data/3w_T_{T}/")
+    if os.path.exists(data_dir):
+        train = torch.load(f"{data_dir}/x_train.pt")
+        train_output = torch.load(f"{data_dir}/y_train.pt")
+        test = torch.load(f"{data_dir}/x_test.pt")
+        test_output = torch.load(f"{data_dir}/y_test.pt")
+    else:
+        data_dir.mkdir(exist_ok=True, parents=True)
+        train, train_output, test, test_output = w3_generator(900, 0.7, overlap_ratio=0)
+        train_output = np.expand_dims(train_output, -1)
+        test_output = np.expand_dims(test_output, -1)
+
+        torch.save(train, data_dir / "x_train.pt")
+        torch.save(train_output, data_dir / "y_train.pt")
+        torch.save(test, data_dir / "x_test.pt")
+        torch.save(test_output, data_dir / "y_test.pt")
+
+    activation = "tanh"  # Tanh RNN
+    hid_dim = 128
+    num_layers = 1
+    input_dim = 3
+    output_dim = 1
+    config = {}
+    config["loss"] = torch.nn.MSELoss()
+    config["optim"] = "Adam"
+    config["lr"] = 0.0001
+
+    dtype = torch.float32
+
+    train_3w(
+        f"{activation}RNN",
+        RNNModel(
+            config=config,
+            hid_dim=hid_dim,
+            num_layers=num_layers,
+            input_dim=input_dim,
+            output_dim=output_dim,
+            return_sequence=False,
+            dtype=32,
+        ),
+        train,
+        train_output,
+        test,
+        test_output,
+        call_backs=[
+            EarlyStopping(
+                monitor="valid_loss",
+                min_delta=1e-10,
+                patience=5,
+                verbose=True,
+                mode="min",
+            )
+        ],
+        devices=1,
+        dtype=dtype,
+    )
